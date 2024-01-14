4542c4979853f9a502f526ed31ee5565dda14259
Ben Graham <grahamben7@gmail.com>
2024-01-14 21:14:07 +0200

    _commit

---
+++ git.sh
@@ -0,0 +1,8 @@
+_commit = "Init Rust Repo"
+
+git add .
+git commit -m _commit
+git push -u origin 
+git remote add origin https://github.com/bengraham-B/rust-tutorial.git
+git branch -M main
+git push -u origin main
